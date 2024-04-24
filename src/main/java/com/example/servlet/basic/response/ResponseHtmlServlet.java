e8197c7111914fe775da7d75aa385e45bea0fdc2
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-25 01:41:47 +0900

    http 응답 데이터 - 단순 텍스트, html

---
+++ src/main/java/com/example/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,30 @@
+package com.example.servlet.basic.response;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /**
+         * http 응답으로 html 을 반환할 때는 content-type 을 text/html 로 지정해야 한다.
+         */
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("  <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
