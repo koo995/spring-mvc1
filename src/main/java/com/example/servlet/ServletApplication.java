3726ee34bae6aee8746cb725489f3ee1d699ba02
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-24 20:19:27 +0900

    Add @ServletComponentScan

--- src/main/java/com/example/servlet/ServletApplication.java
+++ src/main/java/com/example/servlet/ServletApplication.java
@@ -2,7 +2,12 @@ package com.example.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * 자동으로 서블릿을 다 찾아서 등록한다.
+ */
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
