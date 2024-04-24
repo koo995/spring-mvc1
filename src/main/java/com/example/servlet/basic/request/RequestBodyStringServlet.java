07a90fef09357a1a13e3dd3d4a7e33b9fb516d5c
koo995 <107671886+koo995@users.noreply.github.com>
2024-04-24 23:58:46 +0900

    HTTP 요청 데이터 -api 메시지 바디 - 단순텍스트

---
+++ src/main/java/com/example/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,30 @@
+package com.example.servlet.basic.request;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /**
+         * getInputStream 을 통해서 메시지 바디의 내용을 바이트코드로 바로 꺼낼 수 있다.
+         */
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
