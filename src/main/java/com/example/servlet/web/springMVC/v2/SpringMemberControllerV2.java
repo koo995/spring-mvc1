b05e5bb8425f2f7c9a7f1a2bb2f21cc1a868058a
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-09 04:11:34 +0900

    스프링 MVC - 컨트롤러 통합
    @RequestMapping 을 잘 보면 클래스 단위가 아니라 메서드 단위에 적용된 것을 확인할 수 있다. 따라서 컨트롤러 클래스를 유연하게 하나로 통합할 수 있다.
    그러나... ModelAndView 를 만들고 하는게 어딘가 불편하다.

---
+++ src/main/java/com/example/servlet/web/springMVC/v2/SpringMemberControllerV2.java
@@ -0,0 +1,48 @@
+package com.example.servlet.web.springMVC.v2;
+
+import com.example.servlet.domain.member.Member;
+import com.example.servlet.domain.member.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form"); // 여기서도 앞에서 설정했던 뷰리졸버가 적용된다.
+    }
+
+    /**
+     * 여기서 오류가 나타난다.
+     */
+    @RequestMapping()
+    public ModelAndView list() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+}
