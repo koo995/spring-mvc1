403446e462fd9e3efe4cffcfa2d1326ef2b08ec5
koo995 <107671886+koo995@users.noreply.github.com>
2024-05-09 04:32:51 +0900

    개발자들이 더 깔끔하게 만들었다

--- src/main/java/com/example/servlet/web/springMVC/v2/SpringMemberControllerV2.java
+++ src/main/java/com/example/servlet/web/springMVC/v2/SpringMemberControllerV2.java
@@ -4,9 +4,7 @@ import com.example.servlet.domain.member.Member;
 import com.example.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +18,12 @@ public class SpringMemberControllerV2 {
      * 애너테이션 기반의 컨트롤러는 인터페이스가 아니라 딱 고정되어있지 않다. 매우 유연하다.
      * ModelAndView 을 반환해도 되고 String 으로 반환해도 된다.
      */
-    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String list(Model model) {
         List<Member> members = memberRepository.findAll();
 
@@ -35,9 +33,10 @@ public class SpringMemberControllerV2 {
 
     /**
      * 파라미터를 직접 받을 수 있다.
+     * @RequestParam 은 get 쿼리 파라미터, post form 방식을 모두 지원한다.
      * Model 도 받을 수 있다.
      */
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
     public String save(@RequestParam("username") String username,
                              @RequestParam("age") int age,
                              Model model) {
