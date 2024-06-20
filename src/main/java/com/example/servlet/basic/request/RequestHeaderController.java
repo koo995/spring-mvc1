70474c4980efd21900a47cf0a3749fa7a7aae7b3
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-20 19:52:51 +0900

    Http 요청 - 기본, 헤더 조회

---
+++ src/main/java/com/example/servlet/basic/request/RequestHeaderController.java
@@ -0,0 +1,37 @@
+package com.example.servlet.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    /**
+     * MultiValueMap ? Map 과 유사한데 하나의 키에 여러 값을 받을 수 있다.
+     */
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
+}
