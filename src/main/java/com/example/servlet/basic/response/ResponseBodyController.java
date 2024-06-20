714fbf5c5ebab22ef6da07580c87dbf178bdaf13
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-21 03:52:04 +0900

    Http 응답 - Http API, 메시지 바디에 직접 입력

---
+++ src/main/java/com/example/servlet/basic/response/ResponseBodyController.java
@@ -0,0 +1,62 @@
+package com.example.servlet.basic.response;
+
+
+import com.example.servlet.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    /**
+     * 위에는 그냥 문자열을 처리하는 것이였고
+     * 아래부터는 json 을 처리하는 경우다.
+     */
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    /**
+     * @ResponseBody 을 사용하는 것으로 helloData 을 그대로 넘길 수 있다.
+     * 어 ? 상태코드는 어떻게 바꿈? @ResponseStatus 이 제공된다.
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+
+}
