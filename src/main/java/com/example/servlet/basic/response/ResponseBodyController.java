836cba818dddbcadb846c7284be1c3314a9c1995
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-21 03:54:59 +0900

    Http 응답 - Http API, 메시지 바디에 직접 입력 - @RestController
    Http 메시지 컨버터는 스프링 mvc 어디쯤 사용되는 것일까?
    모든 비밀은 애노테이션 기반의 컨트롤러, 그러니까 @RequestMapping 을 처리하는 핸들러 어댑터인 'RequestMappingHandlerAdapter' 에 있다.

--- src/main/java/com/example/servlet/basic/response/ResponseBodyController.java
+++ src/main/java/com/example/servlet/basic/response/ResponseBodyController.java
@@ -6,15 +6,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Slf4j
-@Controller
+@RestController
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
@@ -27,7 +26,6 @@ public class ResponseBodyController {
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
-    @ResponseBody
     @GetMapping("/response-body-string-v3")
     public String responseBodyV3() {
         return "ok";
@@ -36,6 +34,9 @@ public class ResponseBodyController {
     /**
      * 위에는 그냥 문자열을 처리하는 것이였고
      * 아래부터는 json 을 처리하는 경우다.
+     * 기본 문자 처리는 StringJackson2HttpMessageConverter
+     * 기본 객체 처리는 MappingJackson2HttpMessageConverter <- 지금부터는 이 녀석이 사용될 것이다.
+     * 사실 굉장히 많은 HttpMessageConverter 가 등록되어 있다. accept 헤더와 서버의 컨트롤러 반환타입 조합으로 선택된다.(사실은 MediaType 포함 더 고려한 조합으로?)
      */
     @GetMapping("/response-body-json-v1")
     public ResponseEntity<HelloData> responseBodyJsonV1() {
@@ -50,7 +51,6 @@ public class ResponseBodyController {
      * 어 ? 상태코드는 어떻게 바꿈? @ResponseStatus 이 제공된다.
      */
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
     @GetMapping("/response-body-json-v2")
     public HelloData responseBodyJsonV2() {
         HelloData helloData = new HelloData();
