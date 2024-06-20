f343f272d8e5a60516e86621cb27e58502ec36b4
koo995 <107671886+koo995@users.noreply.github.com>
2024-06-20 21:02:36 +0900

    Http 요청 파라미터 - @ModelAttribute
    실제 개발을 하면 요청 파라미터를 받아서 필요한 객체를 만들고 그 객체에 값을 넣어주어야 한다. 이 과정을 완전히 자동화해주는 @ModelAttribute 기능을 제공한다.

--- src/main/java/com/example/servlet/basic/request/RequestParamController.java
+++ src/main/java/com/example/servlet/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package com.example.servlet.basic.request;
 
+import com.example.servlet.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -97,6 +99,33 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    /**
+     * 마치 마법처럼 HelloData 객체가 생성되고, 요청 파라미터의 값도 모두 들어가 있다.
+     * 스프링 MVC 는 @ModelAttribute 가 있으면
+     * 1. HelloData 객체를 생성
+     * 2. 요청 파라미터의 이름으로 HelloData 객체의 프로퍼티(getXxx, setXxx)를 찾는다 그리고 해당 프로퍼티의 setter 를 호출해서 파라미터의 값을 바인딩한다.
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략이 가능하다!
+     * 어? 그런데 @RequestParam 도 생략이 가능한데...?
+     * 스프링은 String, int, Integer 와 같은 단순타입이면 @RequestParam 이 생략된 것으로 판단
+     * 나머지는 @ModelAttribute 가 생략된 것으로 판단(argument resolver 로 지정해둔 타입은 예외!)
+     * HttpServletResponse 같은 것은 argument resolver 로 지정되어 있다.
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
